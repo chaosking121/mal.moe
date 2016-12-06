bcd68f62104b1c61347709b0370d3b5837086517
Arshad Hosein <iarsh930@gmail.com>
2016-12-05 19:32:06 -0500

    Added Profile Screen.

---
+++ app/src/main/java/moe/mal/waifus/activity/ProfileActivity.java
@@ -0,0 +1,14 @@
+package moe.mal.waifus.activity;
+
+import android.os.Bundle;
+
+import moe.mal.waifus.R;
+
+public class ProfileActivity extends GenericActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
